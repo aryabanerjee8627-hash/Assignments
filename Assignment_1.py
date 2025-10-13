44252058807eea2d2002e79da451697017d4bc2e
Wiqi <arya.banerjee8627@gmail.com>
2025-10-13 20:41:14 +0530

    Update Assignment_1 updated.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -4,11 +4,12 @@ a = input ("Enter first number: ")
 b = input ("Enter second number: ")
 print("Addition: ", int(a) + int(b))
 print("Subtraction: ", int(a) - int(b))
-print("Multiplication: ", int(a) * int(b) )
+print("Multiplication: ", int(a) * int(b))
 print("Division: ", float(a) / float(b))
 
 #Task 2
 print("Task 2 : Full name message")
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
-print ("Hello " + first_name + " " + last_name + "! You just delved into Python.")
+print ("Hello " + first_name + " " + last_name + "! Welcome to Python program.")
+
